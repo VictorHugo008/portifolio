2767017c4c442f14ae20842c53ab29d31bd16163
Victor Hugo <vhhugo882@gmail.com>
2026-02-06 09:36:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,6 @@ def contato():
 
 # Executa o servidor
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.eviron.get("PORT", 5000))    
+    app.run(host="0.0.0.0" , port=port, debug=False)
+
