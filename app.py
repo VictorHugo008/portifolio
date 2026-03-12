ac3491872ceb332cd2eb31825cbe3d5c4b959991
Victor Hugo <vhhugo882@gmail.com>
2026-03-12 09:23:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Página de projetos
 @app.route('/projetos')
 def projetos():
     return render_template('projetos.html')
 
-# Página de contato
-@app.route('/contato')
+@app.route('/contato', methods=['GET', 'POST'])
 def contato():
+    if request.method == 'POST':
+        # Aqui você pode pegar os dados: nome = request.form['nome']
+        return "Mensagem enviada com sucesso!"
     return render_template('contato.html')
 
-# Executa o servidor
 if __name__ == '__main__':
-    port = int(os.eviron.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
-
-
+    app.run(debug=True)
